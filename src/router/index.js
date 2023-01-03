c7bce93fe345cd10504c81e8161f8a707d387475
Minh0105 <minhtrn0105@gmail.com>
2023-01-03 17:50:42 +0700

    add Banner for Recipe Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
-import { lazy } from 'react'
-import { HomeLayout } from '../share/layouts'
-const Home = lazy(() => import('../pages/Home/Home'))
-const About = lazy(() => import('../pages/About/About'))
-const MissingRoute = lazy(() => import('../share/components/MissingRoute'))
+import { lazy } from 'react';
+import { HomeLayout } from '../share/layouts';
+const Home = lazy(() => import('../pages/Home/Home'));
+const About = lazy(() => import('../pages/About/About'));
+const MissingRoute = lazy(() => import('../share/components/MissingRoute'));
+const RecipeList = lazy(() => import('../pages/Recipe/RecipeList'));
 
 // ** public routes (no need authen)
 const publicRoutes = [
-    { path: '/', component: Home, title: 'Hôm nay ăn gì', layout: HomeLayout },
-    { path: '/about', component: About, title: 'Giới thiệu' },
-    { path: '*', component: MissingRoute },
-    { path: '/login', component: Home, title: 'Đăng nhập', layout: null },
-]
+  { path: '/', component: Home, title: 'Hôm nay ăn gì', layout: HomeLayout },
+  { path: '/about', component: About, title: 'Giới thiệu' },
+  { path: '/recipe', component: RecipeList, title: 'Giới thiệu' },
+  { path: '*', component: MissingRoute },
+  { path: '/login', component: Home, title: 'Đăng nhập', layout: null },
+];
 
 // ** private routes (need authen + authorization)
-const privateRoutes = [
-]
+const privateRoutes = [];
 
-export { publicRoutes, privateRoutes }
+export { publicRoutes, privateRoutes };
